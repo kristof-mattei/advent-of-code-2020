21c24e649170eb15237f6af1048547d62ac843f9
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2021-12-11 15:04:59 -0800

    fix: updated tests

--- src/day_10/mod.rs
+++ src/day_10/mod.rs
@@ -1,11 +1,11 @@
 use crate::shared::{Day, PartSolution};
 
-fn calculate_step_up_differences(input: &[u32]) -> (u32, u32, u32) {
+fn calculate_step_up_differences(input: &[u8]) -> (u8, u8, u8) {
     let mut stepup_1 = 0;
     let mut stepup_2 = 0;
     let mut stepup_3 = 0;
 
-    let mut copy: Vec<&u32> = input.iter().collect();
+    let mut copy: Vec<&u8> = input.iter().collect();
 
     copy.sort_unstable();
 
@@ -43,17 +43,30 @@ pub struct Solution {}
 
 impl Day for Solution {
     fn part_1(&self) -> PartSolution {
-        let lines: Vec<String> = include_str!("input.txt").lines().map(Into::into).collect();
-        let input: Vec<u32> = lines.iter().map(|s| s.parse::<u32>().unwrap()).collect();
+        let input: Vec<u8> = include_str!("input.txt")
+            .lines()
+            .map(|s| s.parse::<u8>().unwrap())
+            .collect();
 
         let (s1, _, s3) = calculate_step_up_differences(&input);
 
-        PartSolution::U32(s1 * s3)
+        PartSolution::U32(u32::from(s1) * u32::from(s3))
     }
 
     fn part_2(&self) -> PartSolution {
-        todo!()
+        let input: Vec<u8> = include_str!("input.txt")
+            .lines()
+            .map(|s| s.parse::<u8>().unwrap())
+            .collect();
+
+        let permutations = calculate_permutations(&input);
+
+        PartSolution::U64(permutations)
     }
+}
+
+fn calculate_permutations(_input: &[u8]) -> u64 {
+    0
 }
 
 #[cfg(test)]
@@ -69,11 +82,12 @@ mod tests {
         fn outcome() {
             assert_eq!(PartSolution::U32(1820), (Solution {}).part_1());
         }
+
         #[test]
         fn test_example_1() {
-            let input: Vec<u32> = vec!["16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"]
-                .iter()
-                .map(|s| s.parse::<u32>().unwrap())
+            let input: Vec<u8> = include_str!("example1.txt")
+                .lines()
+                .map(|s| s.parse::<u8>().unwrap())
                 .collect();
 
             let (stepup_1, _stepup_2, stepup_3) = calculate_step_up_differences(&input);
@@ -84,14 +98,10 @@ mod tests {
 
         #[test]
         fn test_example_2() {
-            let input: Vec<u32> = vec![
-                "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45",
-                "19", "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34",
-                "10", "3",
-            ]
-            .iter()
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect();
+            let input: Vec<u8> = include_str!("example2.txt")
+                .lines()
+                .map(|s| s.parse::<u8>().unwrap())
+                .collect();
 
             let (stepup_1, _stepup_2, stepup_3) = calculate_step_up_differences(&input);
 
